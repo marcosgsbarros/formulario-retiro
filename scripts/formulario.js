86b3954de6d3056ff7d1096bb4b3d006ec977063
marcosgsbarros <marcosgsbarros@gmail.com>
2024-11-06 12:51:03 -0300

    alteração função salva no localstorage

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -454,7 +454,27 @@ function validarNascimento(campo) {
     // Salva no localStorage e exibe no console
     localStorage.setItem('dadosFormulario', JSON.stringify(dadosFormulario));
     console.log(dadosFormulario);
+    //Enviar uma requisição POST para o servidor com os dados do formulário
+    //url https://hooks.zapier.com/hooks/catch/20646902/251vjp1/
+    fetch('https://hooks.zapier.com/hooks/catch/20646902/251vjp1/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dadosFormulario)
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Erro ao enviar dados do formulário');
+    }
+    ).then(data => {
+        console.log(data);
+
+        
+
         // Redireciona para a próxima página
         window.location.href = 'pagamento.html';
+    });
     });
 });
