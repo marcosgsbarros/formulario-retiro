e9d1fd313762a694020b244ce782a9aa7d8707a0
marcosgsbarros <marcosgsbarros@gmail.com>
2024-11-06 20:33:50 -0300

    funcionalidade de email implementada

--- functions/send_email.js
+++ functions/send_email.js
@@ -1,19 +1,22 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
 export const handler = async (event) => {
   try {
     const { email, nome } = JSON.parse(event.body);
 
-    // Criação do transportador com variáveis de ambiente para segurança
+    // Configuração do transportador de e-mail
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: process.env.EMAIL_USER, // Referencie a variável de ambiente
-        pass: process.env.EMAIL_PASS, // Referencie a variável de ambiente
+        user: process.env.EMAIL_USER, // Usuário (e-mail)
+        pass: process.env.EMAIL_PASS, // Senha de aplicativo do Gmail
       },
     });
 
-    // Conteúdo personalizado do e-mail de confirmação
+    // Configuração do e-mail a ser enviado
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: email,
@@ -22,10 +25,10 @@ export const handler = async (event) => {
         <p>Olá, ${nome}!</p>
         <p>Estamos felizes em confirmar a sua inscrição para o <strong>Retiro Espiritual 2025</strong>.</p>
         <p>Esperamos que este evento seja uma experiência transformadora para você, repleta de momentos de reflexão, aprendizado e comunhão.</p>
-        <p>Se tiver alguma dúvida, entre em contato com a Larissa da organização</p>
+        <p>Se tiver alguma dúvida, entre em contato com a Larissa da organização.</p>
         <br>
-        <a href="https://wa.me/+5527999450744?text=Olá%20Larissa,%20gostaria%20de%20sanar%20algumas%20dúvidas%20sobre%20o%20Retiro%20Espiritual" target="_blank" class="button"">
-                    Falar com Larissa no WhatsApp
+        <a href="https://wa.me/+5527999450744?text=Olá%20Larissa,%20gostaria%20de%20sanar%20algumas%20dúvidas%20sobre%20o%20Retiro%20Espiritual" target="_blank" class="button">
+          Falar com Larissa no WhatsApp
         </a>
         <br>
         <p>Atenciosamente,</p>
@@ -33,7 +36,7 @@ export const handler = async (event) => {
       `,
     };
 
-    // Envie o e-mail
+    // Envio do e-mail
     const info = await transporter.sendMail(mailOptions);
     console.log('Email enviado com sucesso:', info.response);
 
@@ -46,6 +49,6 @@ export const handler = async (event) => {
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Erro ao enviar o email de confirmação.' }),
-    };
-  }
+    };
+  }
 };
